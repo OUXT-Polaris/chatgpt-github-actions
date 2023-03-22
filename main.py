6226a545038e16cdcee72aab678f03db7de2c8eb
Masaya Kataoka <ms.kataoka@gmail.com>
2023-03-23 01:34:51 +0900

    remove unused mode
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- main.py
+++ main.py
@@ -25,70 +25,6 @@ openai.api_key = args.openai_api_key
 g = Github(args.github_token)
 
 
-def files():
-    repo = g.get_repo(os.getenv('GITHUB_REPOSITORY'))
-    pull_request = repo.get_pull(int(args.github_pr_id))
-
-    ## Loop through the commits in the pull request
-    commits = pull_request.get_commits()
-    for commit in commits:
-        # Getting the modified files in the commit
-        files = commit.files
-        for file in files:
-            # Getting the file name and content
-            filename = file.filename
-            content = repo.get_contents(filename, ref=commit.sha).decoded_content
-
-            # Sending the code to ChatGPT
-            response = openai.Completion.create(
-                engine=args.openai_engine,
-                prompt=(f"Explain Code:\n```{content}```"),
-                temperature=float(args.openai_temperature),
-                max_tokens=int(args.openai_max_tokens)
-            )
-
-            # Adding a comment to the pull request with ChatGPT's response
-            pull_request.create_issue_comment(
-                f"ChatGPT's response about `{file.filename}`:\n {response['choices'][0]['text']}")
-
-
-def patch():
-    repo = g.get_repo(os.getenv('GITHUB_REPOSITORY'))
-    pull_request = repo.get_pull(int(args.github_pr_id))
-
-    content = get_content_patch()
-
-    if len(content) == 0:
-        pull_request.create_issue_comment(f"Patch file does not contain any changes")
-        return
-
-    parsed_text = content.split("diff")
-
-    for diff_text in parsed_text:
-        if len(diff_text) == 0:
-            continue
-
-        try:
-            file_name = diff_text.split("+++ b/")[1].splitlines()[0]
-            print(file_name)
-
-            response = openai.Completion.create(
-                engine=args.openai_engine,
-                prompt=(f"Summarize what was done in this diff:\n```{diff_text}```"),
-                temperature=float(args.openai_temperature),
-                max_tokens=int(args.openai_max_tokens)
-            )
-            print(response)
-            print(response['choices'][0]['text'])
-
-            pull_request.create_issue_comment(
-                f"ChatGPT's response about ``{file_name}``:\n {response['choices'][0]['text']}")
-        except Exception as e:
-            error_message = str(e)
-            print(error_message)
-            pull_request.create_issue_comment(f"ChatGPT was unable to process the response about {file_name}")
-
-
 def review():
     repo = g.get_repo(os.getenv('GITHUB_REPOSITORY'))
     pull_request = repo.get_pull(int(args.github_pr_id))
@@ -131,13 +67,6 @@ def get_content_patch():
         raise Exception(response.text)
 
     return response.text
-
-
-if (args.mode == "files"):
-    files()
-
-if (args.mode == "patch"):
-    patch()
 
 if (args.mode == "review"):
     review()
